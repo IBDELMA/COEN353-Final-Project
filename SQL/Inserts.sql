b739ab52e1c611c80ad1821c35d0e35509da923c
JosephHong3145 <dot7201@gmail.com>
2023-04-11 03:55:50 -0400

    fixed date format and zipcode

--- SQL/Inserts.sql
+++ SQL/Inserts.sql
@@ -13,63 +13,63 @@ DELETE FROM Scheduled;
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('BYER723802947583', 'Yero', 'Brent', '03/11/1974', '927 Blueberry Avenue', 'H6Z 1J6', '514-868-5555', 'Canadian Citizen', 'byer@clsc.ca', 'Manager');
+VALUES('BYER723802947583', 'Yero', 'Brent', '1974-03-11', '927 Blueberry Avenue', 'H6Z 1J6', '514-868-5555', 'Canadian Citizen', 'byer@clsc.ca', 'Manager');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('DFLE865495682758', 'Fletcher', 'David', '01/11/1988', '456 Dale Avenue', 'H1Z 1J9', '514-487-4444', 'Permanent Resident', 'dfle@clsc.ca', 'Manager');
+VALUES('DFLE865495682758', 'Fletcher', 'David', '1988-01-11', '456 Dale Avenue', 'H1Z 1J9', '514-487-4444', 'Permanent Resident', 'dfle@clsc.ca', 'Manager');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('EMIL865495682758', 'Miller', 'Emily', '02/12/1989', '567 Euclid Avenue', 'H3C 3C3', '514-487-5555', 'Canadian Citizen', 'emil@clsc.ca', 'Manager');
+VALUES('EMIL865495682758', 'Miller', 'Emily', '1989-02-12', '567 Euclid Avenue', 'H3C 3C3', '514-487-5555', 'Canadian Citizen', 'emil@clsc.ca', 'Manager');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('FARC865495682758', 'Archer', 'Frank', '03/13/1990', '678 Farmhouse Avenue', 'H4D 4D4', '514-487-6666', 'Permanent Resident', 'farc@clsc.ca', 'Manager');
+VALUES('FARC865495682758', 'Archer', 'Frank', '1990-03-13', '678 Farmhouse Avenue', 'H4D 4D4', '514-487-6666', 'Permanent Resident', 'farc@clsc.ca', 'Manager');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('JHUN865495682758', 'Hunter', 'John', '04/14/1991', '789 Johnson Avenue', 'H5E 5E5', '514-487-7777', 'Canadian Citizen', 'jhun@clsc.ca', 'Manager');
+VALUES('JHUN865495682758', 'Hunter', 'John', '1991-04-14', '789 Johnson Avenue', 'H5E 5E5', '514-487-7777', 'Canadian Citizen', 'jhun@clsc.ca', 'Manager');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('ASMI865495682758', 'Smith', 'Alice', '04/12/1981', '123 Apple Avenue', 'H1A 1A1', '514-621-1111', 'Permanent Resident', 'asmi@clsc.ca', 'Doctor');
+VALUES('ASMI865495682758', 'Smith', 'Alice', '1981-04-12', '123 Apple Avenue', 'H1A 1A1', '514-621-1111', 'Permanent Resident', 'asmi@clsc.ca', 'Doctor');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('BSHE865495682758', 'Shepherd', 'Bob', '05/13/1982', '234 Bee Avenue', 'H2B 2B2', '514-621-2222', 'Canadian Citizen', 'bshe@clsc.ca', 'Doctor');
+VALUES('BSHE865495682758', 'Shepherd', 'Bob', '1982-05-13', '234 Bee Avenue', 'H2B 2B2', '514-621-2222', 'Canadian Citizen', 'bshe@clsc.ca', 'Doctor');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('CWHE865495682758', 'Wheeler', 'Charley', '06/14/1983', '345 Cris Avenue', 'H3Z 3J8', '514-621-3333', 'Permanent Resident', 'cwhe@clsc.ca', 'Doctor');
+VALUES('CWHE865495682758', 'Wheeler', 'Charley', '1983-06-14', '345 Cris Avenue', 'H3Z 3J8', '514-621-3333', 'Permanent Resident', 'cwhe@clsc.ca', 'Doctor');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('ROLE865495682758', 'Oley', 'Ricard', '05/15/1992', '131 Isabella Avenue', 'H1Z 1J9', '514-487-3838', 'Canadian Citizen', 'role@clsc.ca', 'Doctor');
+VALUES('ROLE865495682758', 'Oley', 'Ricard', '1992-05-15', '131 Isabella Avenue', 'H1Z 2J0', '514-487-3838', 'Canadian Citizen', 'role@clsc.ca', 'Doctor');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('KWEA865495682758', 'Weaver', 'Karen', '06/16/1993', '890 King Avenue', 'H6F 6F6', '514-487-8888', 'Permanent Resident', 'kwea@clsc.ca', 'Doctor');
+VALUES('KWEA865495682758', 'Weaver', 'Karen', '1993-06-16', '890 King Avenue', 'H6F 6F6', '514-487-8888', 'Permanent Resident', 'kwea@clsc.ca', 'Doctor');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('LANG865495682758', 'Angler', 'Liam', '07/17/1994', '901 Langelier Avenue', 'H7G 7G7', '514-487-9999', 'Canadian Citizen', 'lang@clsc.ca', 'Nurse');
+VALUES('LANG865495682758', 'Angler', 'Liam', '1994-07-17', '901 Langelier Avenue', 'H7G 7G7', '514-487-9999', 'Canadian Citizen', 'lang@clsc.ca', 'Nurse');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('MSTO865495682758', 'Stone', 'Mary', '08/18/1995', '012 Mansion Avenue', 'H8H 8H8', '514-487-0000', 'Permanent Resident', 'msto@clsc.ca', 'Nurse');
+VALUES('MSTO865495682758', 'Stone', 'Mary', '1995-08-18', '012 Mansion Avenue', 'H8H 8H8', '514-487-0000', 'Permanent Resident', 'msto@clsc.ca', 'Nurse');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('NBLA865495682758', 'Black', 'Nelson', '09/19/1996', '123 Niagara Avenue', 'H9J 9J9', '514-487-1111', 'Canadian Citizen', 'nbla@clsc.ca', 'Nurse');
+VALUES('NBLA865495682758', 'Black', 'Nelson', '1996-09-19', '123 Niagara Avenue', 'H9J 9J9', '514-487-1111', 'Canadian Citizen', 'nbla@clsc.ca', 'Nurse');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('OHAG865495682758', 'Hageman', 'Oprah', '10/10/1997', '234 Oppenheimer Avenue', 'H0K 0K0', '514-487-2222', 'Permanent Resident', 'ohag@clsc.ca', 'Nurse');
+VALUES('OHAG865495682758', 'Hageman', 'Oprah', '1997-10-10', '234 Oppenheimer Avenue', 'H0K 0K0', '514-487-2222', 'Permanent Resident', 'ohag@clsc.ca', 'Nurse');
 
 INSERT INTO Employee
 (`Medicare Number`, `Last Name`, `First Name`, `Birth Date`, Address, `Postal Code`, `Phone Number`, Citizenship, Email, `Role`)
-VALUES('PAND865495682758', 'Anderson', 'Peter', '11/11/1998', '345 Palace Avenue', 'H1L 1L1', '514-487-3333', 'Canadian Citizen', 'pand@clsc.ca', 'Nurse');
+VALUES('PAND865495682758', 'Anderson', 'Peter', '1998-11-11', '345 Palace Avenue', 'H1L 1L1', '514-487-3333', 'Canadian Citizen', 'pand@clsc.ca', 'Nurse');
 
 -- -----------------------
 -- Insert Facilities
@@ -101,75 +101,75 @@ VALUES('Canada National Hospital', '514-444-3456', '333 Dale Street', 'H3A 5C5',
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('CLSC Concordia', '514-555-6161', 'BYER723802947583', '01/01/2020', '02/02/2021');
+VALUES('CLSC Concordia', '514-555-6161', 'BYER723802947583', '2020-01-01', '2021-02-02');
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('CLSC Concordia', '514-555-6161', 'ASMI865495682758', '02/02/2020', '03/03/2021');
+VALUES('CLSC Concordia', '514-555-6161', 'ASMI865495682758', '2020-02-02', '2021-03-03');
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('CLSC Concordia', '514-555-6161', 'LANG865495682758', '03/03/2020', '04/04/2021');
+VALUES('CLSC Concordia', '514-555-6161', 'LANG865495682758', '2020-03-03', '2021-04-04');
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('PJC Snowden', '514-444-8123', 'DFLE865495682758', '04/04/2020', '05/05/2021');
+VALUES('PJC Snowden', '514-444-8123', 'DFLE865495682758', '2020-04-04', '2021-05-05');
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('PJC Snowden', '514-444-8123', 'BSHE865495682758', '05/05/2020', '06/06/2021');
+VALUES('PJC Snowden', '514-444-8123', 'BSHE865495682758', '2020-05-05', '2021-06-06');
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('PJC Snowden', '514-444-8123', 'MSTO865495682758', '06/06/2020', '07/07/2021');
+VALUES('PJC Snowden', '514-444-8123', 'MSTO865495682758', '2020-06-06', '2021-07-07');
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Central Hospital', '514-444-1234', 'EMIL865495682758', '02/02/2021', '03/03/2022');
+VALUES('Central Hospital', '514-444-1234', 'EMIL865495682758', '2021-02-02', '2022-03-03');
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Central Hospital', '514-444-1234', 'CWHE865495682758', '03/03/2021', '04/04/2022');
+VALUES('Central Hospital', '514-444-1234', 'CWHE865495682758', '2021-03-03', '2022-04-04');
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Central Hospital', '514-444-1234', 'NBLA865495682758', '04/04/2021', '05/05/2022');
+VALUES('Central Hospital', '514-444-1234', 'NBLA865495682758', '2021-04-04', '2022-05-05');
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Clinic Riverside', '514-444-2345', 'FARC865495682758', '03/03/2022', '04/04/2023');
+VALUES('Clinic Riverside', '514-444-2345', 'FARC865495682758', '2022-03-03', '2023-04-04');
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Clinic Riverside', '514-444-2345', 'ROLE865495682758', '04/04/2022', NULL);
+VALUES('Clinic Riverside', '514-444-2345', 'ROLE865495682758', '2022-04-04', NULL);
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Clinic Riverside', '514-444-2345', 'OHAG865495682758', '05/05/2022', NULL);
+VALUES('Clinic Riverside', '514-444-2345', 'OHAG865495682758', '2022-05-05', NULL);
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Canada National Hospital', '514-444-3456', 'JHUN865495682758', '04/04/2023', NULL);
+VALUES('Canada National Hospital', '514-444-3456', 'JHUN865495682758', '2023-04-04', NULL);
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Canada National Hospital', '514-444-3456', 'KWEA865495682758', '04/05/2023', NULL);
+VALUES('Canada National Hospital', '514-444-3456', 'KWEA865495682758', '2023-04-05', NULL);
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Canada National Hospital', '514-444-3456', 'PAND865495682758', '04/06/2023', NULL);
+VALUES('Canada National Hospital', '514-444-3456', 'PAND865495682758', '2023-04-06', NULL);
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Canada National Hospital', '514-444-3456', 'OHAG865495682758', '04/07/2023', NULL);
+VALUES('Canada National Hospital', '514-444-3456', 'OHAG865495682758', '2023-04-07', NULL);
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Canada National Hospital', '514-444-3456', 'ROLE865495682758', '04/08/2022', NULL);
+VALUES('Canada National Hospital', '514-444-3456', 'ROLE865495682758', '2023-04-08', NULL);
 
 INSERT INTO Employed
 (`Facility Name`, `Facility Phone Number`, `Medicare Number`, `Start Date`, `End Date`)
-VALUES('Canada National Hospital', '514-444-3456', 'NBLA865495682758', '04/09/2022', NULL);
+VALUES('Canada National Hospital', '514-444-3456', 'NBLA865495682758', '2023-04-09', NULL);
 
 
 -- -----------------------
@@ -209,7 +209,7 @@ VALUES('H3Z 3J8', 'Montreal', 'Quebec');
 
 INSERT INTO PostalCode
 (`Postal Code`, City, Province)
-VALUES('H0K 0K0', 'Montreal', 'Quebec');
+VALUES('H1Z 2J0', 'Montreal', 'Quebec');
 
 INSERT INTO PostalCode
 (`Postal Code`, City, Province)
@@ -229,7 +229,7 @@ VALUES('H9J 9J9', 'Montreal', 'Quebec');
 
 INSERT INTO PostalCode
 (`Postal Code`, City, Province)
-VALUES('H3A 4B4', 'Montreal', 'Quebec');
+VALUES('H0K 0K0', 'Montreal', 'Quebec');
 
 INSERT INTO PostalCode
 (`Postal Code`, City, Province)
