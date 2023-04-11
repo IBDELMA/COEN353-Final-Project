3d9b7c6a16436b52cfa1050e00880409cd7da084
Jonathan Perlman <31137025+jonman5@users.noreply.github.com>
2023-04-11 03:15:37 -0400

    Add infection triggers

---
+++ SQL/InfectionTriggers.sql
@@ -0,0 +1,38 @@
+/* 
+“If a doctor or a nurse gets infected by COVID-19, then the system should automatically cancel 
+all the assignments for the infected employee for two weeks from the date of infection. 
+Also, the system should send an email to inform/track all the doctors and nurses who have been in 
+contact by having the same schedule as the infected employee. Each email should have as a subject “Warning” 
+and as a body “One of your colleagues that you have worked with in the past two weeks have been infected with 
+COVID-19”. 
+*/
+
+DROP TRIGGER IF EXISTS jbc353_4.InfectionInsert;
+DELIMITER $$
+CREATE TRIGGER jbc353_4.InfectionInsert
+BEFORE INSERT ON jbc353_4.Infection 
+FOR EACH ROW
+BEGIN
+	IF NEW.Type = 'COVID-19'
+	THEN
+		DELETE FROM Scheduled s	
+		WHERE s.`Employee Medicare Number` = NEW.`Employee Medicare Number` 
+		AND (DATEDIFF(s.`Date`, NEW.Date) <= 14 AND DATEDIFF(s.`Date`, NEW.Date) >= 0);
+	END IF; 
+END;$$ 
+delimiter ;
+
+DROP TRIGGER IF EXISTS jbc353_4.InfectionUpdate;
+DELIMITER $$
+CREATE TRIGGER jbc353_4.InfectionUpdate
+BEFORE UPDATE ON jbc353_4.Infection 
+FOR EACH ROW
+BEGIN
+	IF NEW.Type = 'COVID-19'
+	THEN
+		DELETE FROM Scheduled s	
+		WHERE s.`Employee Medicare Number` = NEW.`Employee Medicare Number` 
+		AND (DATEDIFF(s.`Date`, NEW.Date) <= 14 AND DATEDIFF(s.`Date`, NEW.Date) >= 0);
+	END IF; 
+END;$$ 
+delimiter ;
