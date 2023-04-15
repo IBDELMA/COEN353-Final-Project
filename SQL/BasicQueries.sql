92d421775b25848dc32463740d10e62e05595c6f
Jonathan Perlman <31137025+jonman5@users.noreply.github.com>
2023-04-14 23:53:47 -0400

    Add Scheduled and PostalCode basic queries

--- SQL/BasicQueries.sql
+++ SQL/BasicQueries.sql
@@ -21,3 +21,20 @@ SELECT `Type`, `Date`, `Employee Medicare Number` FROM Infection;
 INSERT INTO Infection (`Type`, `Date`, `Employee Medicare Number`) VALUES('', '', '');
 UPDATE Infection SET  WHERE `Type`='' AND `Date`='' AND `Employee Medicare Number`='';
 DELETE FROM Infection WHERE `Type`='' AND `Date`='' AND `Employee Medicare Number`='';
+
+-- Scheduled basic queries (#5)
+SELECT `Facility Name`, `Facility Phone Number`, `Employee Medicare Number`, `Start Time`, `End Time`, `Date` FROM Scheduled;
+INSERT INTO Scheduled (`Facility Name`, `Facility Phone Number`, `Employee Medicare Number`, `Start Time`, `End Time`, `Date`) VALUES('', '', '', '', '', '');
+UPDATE Scheduled SET `End Time`='' WHERE `Facility Name`='' AND `Facility Phone Number`='' AND `Employee Medicare Number`='' AND `Start Time`='' AND `Date`='';
+DELETE FROM Scheduled WHERE `Facility Name`='' AND `Facility Phone Number`='' AND `Employee Medicare Number`='' AND `Start Time`='' AND `Date`='';
+
+-- Schedule insert that will not work (#5) (returns an SQL error with message: "Scheduling time conflict. Unable to add this schedule for this time.")
+INSERT INTO Scheduled
+(`Facility Name`, `Facility Phone Number`, `Employee Medicare Number`, `Start Time`, `End Time`, `Date`)
+VALUES('Canada National Hospital', '514-444-3456', 'ROLE865495682758', '10:00', '13:00', '2023-04-08');
+
+-- PostalCode Basic Queries
+SELECT `Postal Code`, City, Province FROM PostalCode;
+INSERT INTO PostalCode (`Postal Code`, City, Province) VALUES('', '', '');
+UPDATE PostalCode SET City='', Province='' WHERE `Postal Code`='';
+DELETE FROM PostalCode WHERE `Postal Code`='';
