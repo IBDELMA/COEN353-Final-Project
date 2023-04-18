e3e4c489e61b564cfa78fa446dea0a79573b966c
Jonathan Perlman <31137025+jonman5@users.noreply.github.com>
2023-04-17 21:47:44 -0400

    Added working log insertion from email client

--- email-client.php
+++ email-client.php
@@ -1,5 +1,6 @@
 <?php
 include "init_db.php";
+date_default_timezone_set('America/Toronto');
 mysqli_query($db, "LOCK TABLES MailStack READ");
 $r = mysqli_query($db, "SELECT * FROM MailStack");
 // MailStack: {`Facility Name`, Recipient, Subject, Content}
@@ -8,18 +9,34 @@ $assoc = mysqli_fetch_assoc($r);
 if (!$assoc){
     mysqli_free_result($r);
     mysqli_query($db, "UNLOCK TABLES");
-    exit("No emails to send at this moment.");
+    exit("No emails to send at this moment.\n");
 }
+
 $i = 0;
+$logInsertArray = array();
 while (!is_null($assoc)){
-    $content = $assoc["Content"];
-    $subject = $assoc["Subject"] . " TO: " . $assoc["Recipient"];
+    $facilityName = mysqli_real_escape_string($db, $assoc["Facility Name"]);
+    $recipient = mysqli_real_escape_string($db, $assoc["Recipient"]);
+    $subject = mysqli_real_escape_string($db, $assoc["Subject"]);
+    $content = mysqli_real_escape_string($db, $assoc["Content"]);
+
     $shellOutput = shell_exec("echo \"$content\" | mail -s \"$subject\" jonnam2@gmail.com");
+    $logInsertQuery = "INSERT INTO Log (`Date`, Receiver, Sender, Subject, Body) VALUES(CURDATE(), '$recipient', '$facilityName', '$subject', '$content')";
+    $logInsertArray[$i] = $logInsertQuery;
     $i++;
     $assoc = mysqli_fetch_assoc($r);
 }
 mysqli_free_result($r);
+
 mysqli_multi_query($db, "UNLOCK TABLES; DELETE FROM MailStack;");
-date_default_timezone_set('America/Toronto');
-echo("$i emails were sent at " . date('m/d/Y h:i:s a'));
+while (mysqli_next_result($db)) {;}
+
+foreach ($logInsertArray as $query){
+    echo($logInsertQuery. "\n");
+    $res = mysqli_query($db, $logInsertQuery);
+    if ($res){
+        echo("Success!\n");
+    }
+}
+echo("$i emails were sent at " . date('m/d/Y h:i:s a') . "\n");
 ?>
