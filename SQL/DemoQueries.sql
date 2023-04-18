c4a8adcff46f499cec84f224a1ca2161bc557ab8
Jonathan Perlman <31137025+jonman5@users.noreply.github.com>
2023-04-18 15:20:23 -0400

    Add some demo queries and inserts

---
+++ SQL/DemoQueries.sql
@@ -0,0 +1,11 @@
+INSERT INTO Infection
+(`Type`, `Date`, `Employee Medicare Number`)
+VALUES('COVID-19', '2023-04-14', 'ROLE865495682758');
+
+INSERT INTO Infection
+(`Type`, `Date`, `Employee Medicare Number`)
+VALUES('COVID-19', '2023-04-03', 'PAND865495682758');
+
+INSERT INTO Scheduled
+(`Facility Name`, `Facility Phone Number`, `Employee Medicare Number`, `Start Time`, `End Time`, `Date`)
+VALUES('Canada National Hospital', '514-444-3456', 'ROLE865495682758', '10:00', '13:00', '2023-04-08');
